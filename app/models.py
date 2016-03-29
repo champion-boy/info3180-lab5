09ae58a47b77cd60d4b9b12e9569c42026feb36f
champion-boy <romarioomartin@hotmail.com>
2016-03-29 19:31:06 -0400

    Update models.py

--- app/models.py
+++ app/models.py
@@ -3,11 +3,33 @@ class Myprofile(db.Model):
     id = db.Column(db.Integer, primary_key=True)     
     first_name = db.Column(db.String(80))     
     last_name = db.Column(db.String(80)) 
-    nickname = db.Column(db.String(80), unique=True)    
-    email = db.Column(db.String(120), index=True, unique=True)
- 
-
+    username = db.Column(db.String(80), unique=True) 
+    password = db.Column(db.String(80), unique=True)     
     
+    def is_authenticated(self):
+        return True
+
+    def is_active(self):
+        return True
+
+    def is_anonymous(self):
+        return False
+
+    def get_id(self):
+        try:
+            return unicode(self.id)  # python 2 support
+        except NameError:
+            return str(self.id)  # python 3 support
+
+    def __repr__(self):
+        return '<User %r>' % (self.username)
+        
+class User(db.Model):     
+    id = db.Column(db.Integer, primary_key = True)
+    email = db.Column(db.String(64), unique=True, index=True)
+    username = db.Column(db.String(64), unique=True, index=True)
+    password_hash = db.Column(db.String(128))
+        
     def is_authenticated(self):
         return True
 
